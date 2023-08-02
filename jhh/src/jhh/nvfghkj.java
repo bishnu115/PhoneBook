b084dcf09fcc62fdad14f6efb9bc937a6256da69
Lenovo <Lenovo@DESKTOP-4SHEN3F>
2023-08-02 14:33:39 +0530

    ewuwh

---
+++ jhh/src/jhh/nvfghkj.java
@@ -0,0 +1,15 @@
+package jhh;
+
+public class nvfghkj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public void reclogin()
+		{
+			username.sendKeys("recep@gmail.com");	
+			passwoard.sendKeys("123456");
+			login.click();
+		}
+	}
+
+}
